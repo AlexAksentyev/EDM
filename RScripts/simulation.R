32d28f2b5bc57dda9965c1d113268df348d01b79
Alex Aksentyev <alexaksentyev@gmail.com>
2016-11-30 18:33:15 +0100

    my prediction regarding the samplings' efficiencies was correct; SEUni/SEMod in (1.91--1.97)

--- RScripts/simulation.R
+++ RScripts/simulation.R
@@ -1,4 +1,6 @@
+rm(list=ls(all=TRUE))
 library(dplyr); library(plyr)
+library(reshape2)
 fancy <- function(x) formatC(x, 4, format = "e")
 
 ## I should show that my formula for var omega is the correct one
@@ -10,12 +12,12 @@ phi = pi/2 #initial signal phase
 w = 2*pi*.005#2*pi*120e3*.74e-4 # estimated frequency
 .dcs <- function(x) N0 * (1 + P*sin(w*x + phi)) # structural model/expectation
 
-w.guess = rnorm(1, w, 0*w) # if we do modulated sampling, we must guess the signal frequency; this line models the guessing
+w.guess = rnorm(1, w, .1*w) # if we do modulated sampling, we must guess the signal frequency; this line models the guessing
 
 Ttot = 1000 #total measurement time
-fs = w/(2*pi)*sqrt(8) #sampling frequency
+fs = w/(2*pi)*sqrt(4.3) #sampling frequency
 t = seq(0, Ttot, by = 1/fs);#uniform sampling time 
-t2 = seq(0, Ttot, by = .33/fs); plot(.dcs(t2)~t2, type="o"); points(.dcs(t)~t, col="blue")
+t2 = seq(0, Ttot, by = .33/fs); plot(.dcs(t2)~t2, type="o", xlab="Time, sec", ylab="Signal, counts"); points(.dcs(t)~t, col="blue")
 t2 <- t2[.dcs(t2) > N0*(1 - P*.5) & .dcs(t2) < N0*(1 + P*.5)]; points(.dcs(t2)~t2, col="red"); 
 abline(h=N0*(1 + c(-.5,0,.5)*P), col="red", lty=c(2,3,2))
 x = seq(0,Ttot,length.out=1000); lines(.dcs(x)~x, lty=4)
@@ -26,22 +28,29 @@ rm(t2)
 
 Es = llply(t, function(x) {.dcs(x) -> e; names(e) <- x; e}) # expected values for the two samplings
 
-Ntrial = 5 #number of trials
+
+#### modeling ####
+Ntrial = 500 #number of trials
 matrix(rnorm(Ntrial*l, sd=errS), ncol=Ntrial) -> s
 llply(Es, function(e){e+s -> x; rownames(x) <- names(e); x})->s
 
-## example of one trial
-trl = llply(s, function(e) data.frame(t = as.numeric(rownames(e)), m = e[,3]))
-ci = .95; z = abs(qnorm((1-ci)/2))
-llply(trl, function(e){
-  nls(m~ n*(1 + p*sin(frq*t + phi)), data=e, start=list(n = 5000, p = 1, frq=w.guess)) -> m3
-  frq = coef(m3)["frq"]; sef = sqrt(vcov(m3)["frq","frq"])
-  confint(m3) %>% cbind("SE" = sqrt(diag(vcov(m3))), "True" = c(N0, P, w)) %>% fancy
-})
+melt(s) -> s; names(s) <- c("Time","Trl","Sgl", "Type")
 
+## for all trials ####
+s %>% ddply(.(Type, Trl), function(trl){
+  nls(Sgl~ n*(1 + p*sin(frq*Time + phi)), data=trl, start=list(n = 5000, p = 1, frq=w.guess)) -> m3
+  .est = coef(m3); names(.est) <- paste("X",names(.est), sep=".")
+  .ster = sqrt(diag(vcov(m3))); names(.ster) <- paste("SE",names(.ster), sep=".")
+  data.frame(c(.est, .ster)%>%t)
+}) -> .stats
 
+.se <- function(x) sd(x)/sqrt(length(x))
 
-######
-# adply(s, 2, function(trl){
-#   nls(trl~ n*(1 + p*sin(frq*t)), start=list(n = 5000, p = 1, frq=rnorm(1, w, .1*w)))
-# })
+library(ggplot2)
+ggplot(.stats, aes(X.frq, col=Type)) + geom_density() + 
+  geom_vline(xintercept=w, linetype=2)+ 
+  theme_bw() + labs(x=expression(hat(omega)))
+
+## comparing the efficiencies
+daply(.stats, "Type", function(x) .se(x$X.frq)) -> x
+x["uni"]/x["mod"]
