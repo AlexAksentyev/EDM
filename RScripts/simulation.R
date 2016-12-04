9b1645486dcb5b0b59f0d30ac4ecd50ddc0f2dce
AlexAksentyev <alexaksentyev@gmail.com>
2016-12-04 01:33:37 +0100

    minor changes; added multitrial SE vs SEAN comparison

--- RScripts/simulation.R
+++ RScripts/simulation.R
@@ -42,8 +42,6 @@ fancy <- function(x) formatC(x, 4, format = "e")
 .simul <- function(data) data %>% ddply(.(Type, Trl), .fit, .parallel=FALSE) %>% 
   mutate(B.frq = X.frq - w0, RB.frq = B.frq/w0, Type = as.factor(Type)) 
 
-
-
 #### preparations ####
 ## I should show that my formula for var omega is the correct one
 w0=3; phi=pi/36; N0=6730; P=.8 # signal
@@ -51,50 +49,52 @@ fs=5000; comptn=.33; w.g=rnorm(1,3,0) ## sampling
 errS = 3e-2*N0*P #absolute measurement error
 
 ## modeling ##
-# the first two will have to be adjusted b/c now I return a list of data frames from .sample
-## 1) trials with different error ####
-Ntrial = 5 #number of trials
-Es = .sample(3) # expected values for the two samplings
-l = nrow(Es)
-Es[rep(seq(l),Ntrial),] %>% mutate(Trl = rep(seq(Ntrial), each=l), Sgl = Sgl + rnorm(Ntrial*l, sd=errS)) -> s
-.stats <- .simul(s)
 
-## plotting results ##
-ggplot(.stats, aes(B.frq, col=Type)) + geom_density() +
-  theme_bw() + labs(x=expression(bar(omega) - omega))
+## 1) trials with different error ####
+Ntrial = 10 #number of trials
+Es = .sample(30) # expected values for the two samplings
+l = nrow(Es)
+Es[rep(seq(l),Ntrial),] %>% mutate(Trl = rep(seq(Ntrial), each=l), Sgl = Sgl + rnorm(Ntrial*l, sd=errS)) %>%
+  ddply(.(Type, Trl), function(trl) .fit(trl)%>%mutate(SEAN.frq = .compVarF(trl))) -> .stats
+
+.stats%>%group_by(Type) %>% dplyr::summarise(NUM = n(), SE = sd(X.frq), SEAN = SEAN.frq[1], RD = (SE-SEAN)/SEAN)
 
 ## comparing the efficiencies
 daply(.stats, "Type", function(x) .se(x$X.frq)) -> x
 print(x)
 c("SE ratio" = x["uniform"]/x["modulated"], "wg/w0" = w.g/w0) %>% print
 
+## plot of sample points on signal
 x = filter(s, Trl==1) %>% mutate(Sgl.o = .dcs(Time), errY = Sgl-Sgl.o) %>% slice(seq(1,2*l, length.out=250))
 ggplot(x, aes(Time, Sgl)) + geom_pointrange(aes(ymin=Sgl-errS, ymax=Sgl+errS,col=Type), size=.3) + theme_bw() + 
   geom_line(aes(Time, Sgl), data.frame(Time = seq(0, Ttot, by=1/fs)) %>% mutate(Sgl=.dcs(Time)))
 
-ggplot(.stats) + geom_boxplot(aes(Type, B.frq)) + theme_bw() + labs(y=expression(sigma[hat(omega)]))
-
 ## 2) checking the growth of omega se with total time ####
 Es <- .sample(15); nrow(Es)->l
-mutate(Es, Trl=1, Sgl = Sgl + rnorm(l, sd=errS))  -> s
+mutate(Es, Sgl = Sgl + rnorm(l, sd=errS))  -> s
 
 (1:10)*l/length(unique(s$Type))/10 -> m
 llply(m, function(i) s%>%ddply("Type", function(x) slice(x,1:i))) -> s
-ldply(s, function(df) .simul(df)%>%mutate(NUM = nrow(df), SEAN.frq = .compVarF(df))) %>% 
-  mutate(MEAN.SE = SE.frq/sqrt(NUM))->.stats
+ldply(s, function(df) df%>%ddply("Type", .fit)%>%mutate(NUM = nrow(df), SEAN.frq = .compVarF(df))) %>% 
+  mutate(SE.MEAN = SE.frq/sqrt(NUM))->.stats
 
-ggplot(.stats, aes(NUM, MEAN.SE, col=Type)) + geom_point() + 
+ggplot(.stats, aes(NUM, SE.MEAN, col=Type)) + geom_point() + 
   scale_y_log10() + scale_x_log10() +
   theme_bw() + labs(x="Sample size",y=expression(sigma[bar(omega)])) + theme(legend.position="top")
 
-ggplot(mutate(.stats, Dev = SE.frq-SEAN.frq), aes(NUM, Dev, col=Type)) + geom_point() + 
-  theme_bw() + labs(x="Sample size", y="Simulation - Analytic SE") + theme(legend.position="top")
+ggplot(mutate(.stats, Dev = (SE.frq-SEAN.frq)/SEAN.frq), aes(NUM, abs(Dev), col=Type)) + geom_point() + 
+  theme_bw() + labs(x="Sample size", y="(Simulation - Analytic)/Analytic SE") + theme(legend.position="top")
 
-  ## 3) checking the analytical formula ####
+ggplot(.stats, aes(SEAN.frq, SE.frq, col=Type)) + geom_point() + 
+  theme_bw() + labs(x="Analytic SE", y="Simulation SE") + theme(legend.position="top") + 
+  geom_abline(slope=1,intercept=0) + 
+  scale_y_log10() + scale_x_log10()
+
+## 3) checking the analytical formula ####
 ## the formula is
 ## SEw = SE error / sqrt(sum xj * (sum ti^2 xi/sum xj) - (sum ti xi/sum xj)^2)
 Es = .sample(10); l = nrow(Es[[1]])
 rnorm(l, sd=errS)%>%rep(length(Es)) -> s
-ldply(Es, .id="Type") %>% mutate(Sgl = Sgl + s, Trl=1) -> s
-.stats <- .simul(s) %>% mutate(SEAN.frq = daply(s, "Type", .compVarF))
+ldply(Es, .id="Type") %>% mutate(Sgl = Sgl + s) -> s
+.stats <- ddply(s,"Type", .fit) %>% mutate(SEAN.frq = daply(s, "Type", .compVarF))
  
