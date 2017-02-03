2598ab5a3df0741984e67830674d766f303f672d
Alex Aksentyev <alexaksentyev@gmail.com>
2017-02-03 19:14:38 +0100

    observe the lagging frequency

--- RScripts/Signal.R
+++ RScripts/Signal.R
@@ -12,9 +12,16 @@ p = rnorm(np, p0, 1e0) # initial phases
 
 Pproj <- function(df, x) colSums(cos(df$wFreq%o%x + df$Phi))
 
-df = data.frame(wFreq=w, Phi=p)
-df <- data.frame(Time=seq(0,500,.5/w0)) %>% mutate(Sgl=Pproj(df,Time))
+Tstt = 0; Ttot=500
+df.p = data.frame(wFreq=w, Phi=p)
+df.s = data.frame(Time=seq(Tstt,Ttot,.5/w0)) %>% mutate(Sgl=Pproj(df.p,Time))
 
-ggplot(df, aes(Time, Sgl)) + geom_line() + geom_hline(yintercept = c(min(df$Sgl), max(df$Sgl)), col="red") +
-  theme_bw()
+Ntot = floor(.5*(w0*Ttot+p0)/pi)
+Nstt = floor(.5*(w0*Tstt+p0)/pi)
+tn = (2*pi*Nstt:Ntot-p0)/w0
 
+ggplot(df.s, aes(Time, Sgl)) + geom_line() + geom_hline(yintercept = c(min(df.s$Sgl), max(df.s$Sgl)), col="red") +
+  theme_bw() + geom_point(aes(col="red"), data=data.frame(Time=tn, Sgl=Pproj(df.p,tn)), show.legend = FALSE)
+
+
+# the signal frequency is determined by the MODE of the distribution
