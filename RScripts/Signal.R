30e5cc2925caa9e1bf0c44f3d4e9572e82f3270a
AlexAksentyev <alexaksentyev@gmail.com>
2017-02-04 16:16:53 +0100

    minor changes

--- RScripts/Signal.R
+++ RScripts/Signal.R
@@ -29,17 +29,17 @@ bimodalDistFunc <- function (n,cpct, mu1, mu2, sig1, sig2) {
 np = 1000 # number of particles in bunch
 w0 = 3; f0 = w0/2/pi # frequency of the reference particle
 p0 = pi/3 #phase of the reference particle
-sdw = w0*3e-3
-sdp = p0*3e-1
+sdw = w0*1e-3
+sdp = p0*3e-2
 dis = "bi"
 
 ## particle distributions ####
 df.p = data.frame(
   wFreq = switch(dis,
-    "skew" = skewedDistFunc(np, w0, sdw, 5),
+    "skew" = skewedDistFunc(np, w0, sdw, 10),
     "bi" = bimodalDistFunc(np,.5,w0-3*sdw,w0+3*sdw,sdw,sdw)
   ), 
-  Phi = rnorm(np, p0, sdp)
+  Phi = rnorm(np, p0, sdp) # bimodalDistFunc(np,0,p0-3*sdp,p0+3*sdp,sdp,sdp)
 )
 
 par(mfrow=c(2,1))
@@ -50,17 +50,18 @@ par(mfrow=c(1,1))
 ## computing signal ####
 Pproj <- function(df, x) colSums(cos(df$wFreq%o%x + df$Phi))
 
-Tstt = 0; Ttot=500; dt = .5/w0
+Tstt = 0; Ttot=1000; dt = .5/w0 #.5 to satisfy the Nyquist condition
 df.s = data.frame(Time=seq(Tstt,Ttot,dt)) %>% mutate(Sgl=Pproj(df.p,Time))
 
-## computing peaks ####
+## computing signal peaks ####
 Ntot = floor(.5*(w0*Ttot+p0)/pi)
 Nstt = floor(.5*(w0*Tstt+p0)/pi)
 tnu = (2*pi*Nstt:Ntot-p0)/w0
 tnd = tnu+pi/w0
 
 ## plotting signal ####
-ggplot(df.s, aes(Time, Sgl)) + geom_line() + geom_hline(yintercept = c(min(df.s$Sgl), max(df.s$Sgl)), col="red") +
+ggplot(df.s, aes(Time, Sgl)) + geom_line() + 
+  geom_hline(yintercept = c(min(df.s$Sgl), max(df.s$Sgl)), col="red") +
   theme_bw() + 
   geom_point(aes(col=Side), 
              data=data.frame(
@@ -72,8 +73,10 @@ ggplot(df.s, aes(Time, Sgl)) + geom_line() + geom_hline(yintercept = c(min(df.s$
 
 ## spectral analysis ####
 s = ts(df.s$Sgl, start=Tstt, end=Ttot, deltat=dt)
-spec.ar(s,plot = FALSE) -> sps
+spec.pgram(s,plot = FALSE) -> sps
 sps <- data.frame(Freq=sps$freq, Pow=sps$spec) %>% mutate(wFreq=2*pi*Freq)
-filter(sps, Freq>.35, Freq<.6) %>% ggplot(aes(wFreq, Pow))+geom_bar(stat="identity", size=.5) + 
+
+filter(sps, wFreq>w0-5*sdw, wFreq<w0+5*sdw) %>% ggplot(aes(wFreq, Pow))+
+  geom_bar(stat="identity", width=1e-3) + 
   theme_bw() + labs(x=expression(omega)) +
   geom_vline(xintercept = w0, col="red")
