ba87aef15b54676e27a48845e087e63aebb0f504
Alex Aksentyev <alexaksentyev@gmail.com>
2017-02-17 18:56:29 +0100

    implemented spectrum analysis via Welch's method

--- Python/CSignal.py
+++ Python/CSignal.py
@@ -3,6 +3,7 @@ import numpy as np
 import multiprocessing
 from multiprocessing import pool
 from scipy.optimize import curve_fit
+from scipy.signal import welch
 
 class Signal:
     """Contains fields and methods of a signal
@@ -14,6 +15,7 @@ class Signal:
     Signal=None
     specPts=None
     Model=None
+    PSD=None
     __NSmpl=None
     
     def __init__(self, bunch, smpl_pts):
@@ -28,4 +30,18 @@ class Signal:
     def addNoise(self, rerr):
         self.Signal = self.Signal.assign(Noise = self.Signal.Val*np.random.normal(0, rerr, len(self.Signal)))
         self.Signal = self.Signal.assign(ValNs = self.Signal.Val+self.Signal.Noise)
+        
+    def Spectrum(self):
+        fs = 1/(self.Signal.Time[2] - self.Signal.Time[1])
+        psd = welch(self.Signal.ValNs, fs)
+        self.PSD = pandas.DataFrame({
+            "Freq": psd[0],
+            "Pow" : psd[1]
+        })
+        self.PSD = self.PSD.assign(wFreq = self.PSD.Freq*2*np.pi)
+        #self.Spectrum = pandas.DataFrame({
+        #    "Pow": np.abs(np.fft.fft(self.Signal.ValNs))**2,
+        #    "Freq": np.fft.fftfreq(len(self.Signal), stp)
+        #}).sort_values('Freq')
+        
         
--- Python/test.py
+++ Python/test.py
@@ -1,11 +1,12 @@
 from ggplot import *
 import numpy as np
+import matplotlib.pyplot as plt
 import CBunch
 import CSignal
 
-b1 = CBunch.Bunch(Npart=1e3, wDist="norm")
-#at = numpy.arange(0,2000, .95*np.pi/b1.Synch["wFreq"])
-at = np.linspace(0,2000, 1e3)
+b1 = CBunch.Bunch(Npart=3, wDist="norm")
+at = numpy.arange(0,2000, .95*np.pi/b1.Synch["wFreq"])
+#at = np.linspace(0,2000, 1e3)
 
 s1 = CSignal.Signal(b1, at)
 
@@ -15,3 +16,8 @@ ggplot(s1.Signal, aes(x="Time",y="ValNs")) + geom_point(color="red", size=5) + g
 
 f = lambda t, l,w,p: 1e3*np.exp(l*t)*np.sin(w*t + p)
 s1.fit(f, [-1e-4, s1.Bunch.Synch["wFreq"], 0])
+
+s1.Spectrum()
+
+plt.semilogy(s1.PSD.wFreq, s1.PSD.Pow)
+
