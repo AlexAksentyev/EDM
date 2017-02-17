25be9067d340b6a7309073b6d9a93fe4eca7b83b
Alex Aksentyev <alexaksentyev@gmail.com>
2017-02-17 18:18:15 +0100

    implemented fitting

--- Python/CSignal.py
+++ Python/CSignal.py
@@ -1,8 +1,8 @@
 import pandas
-import numpy
+import numpy as np
 import multiprocessing
 from multiprocessing import pool
-
+from scipy.optimize import curve_fit
 
 class Signal:
     """Contains fields and methods of a signal
@@ -13,7 +13,7 @@ class Signal:
     Bunch=None
     Signal=None
     specPts=None
-    ModelCoef=None
+    Model=None
     __NSmpl=None
     
     def __init__(self, bunch, smpl_pts):
@@ -21,4 +21,11 @@ class Signal:
         self.Signal = self.Bunch.project(smpl_pts)
         self._NSmpl = 1
         
-
+    def fit(self, FUN, Guess):
+        self.Model = curve_fit(FUN, self.Signal.Time, self.Signal.ValNs, p0=Guess)
+        return {"Estimate" : self.Model[0], "SE" : np.sqrt(np.diag(self.Model[1]))}
+        
+    def addNoise(self, rerr):
+        self.Signal = self.Signal.assign(Noise = self.Signal.Val*np.random.normal(0, rerr, len(self.Signal)))
+        self.Signal = self.Signal.assign(ValNs = self.Signal.Val+self.Signal.Noise)
+        
--- Python/test.py
+++ Python/test.py
@@ -1,10 +1,17 @@
 from ggplot import *
-import numpy
+import numpy as np
 import CBunch
 import CSignal
 
-b1 = CBunch.Bunch(Npart=10, wDist="norm")
-at = numpy.arange(0,2000, .95*numpy.pi/b1.Synch["wFreq"])
+b1 = CBunch.Bunch(Npart=1e3, wDist="norm")
+#at = numpy.arange(0,2000, .95*np.pi/b1.Synch["wFreq"])
+at = np.linspace(0,2000, 1e3)
 
 s1 = CSignal.Signal(b1, at)
-ggplot(s1.Signal, aes(x="Time",y="Val")) + geom_line(linetype="dotted", color="blue", size=.5) + theme_bw()
+
+
+s1.addNoise(3e-2)
+ggplot(s1.Signal, aes(x="Time",y="ValNs")) + geom_point(color="red", size=5) + geom_line(aes(x="Time", y="Val"), color="gray", size=.5) + theme_bw()
+
+f = lambda t, l,w,p: 1e3*np.exp(l*t)*np.sin(w*t + p)
+s1.fit(f, [-1e-4, s1.Bunch.Synch["wFreq"], 0])
