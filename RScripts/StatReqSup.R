59752568fda5e9343bb1922a651a11a4e30a2458
Alex Aksentyev <alexaksentyev@gmail.com>
2017-03-01 19:26:06 +0100

    testing the effects of modulation

--- RScripts/StatReqSup.R
+++ RScripts/StatReqSup.R
@@ -1,5 +1,7 @@
 library(ggplot2)
 
+rm(list=ls(all=TRUE))
+
 source("./RScripts/CModel.R")
 source("./RScripts/CSampling.R")
 
@@ -7,19 +9,45 @@ lblfnt = 20
 thm = theme_bw() + theme(axis.text=element_text(size=lblfnt), axis.title=element_text(size=lblfnt), 
                          legend.title=element_text(size=lblfnt), legend.text=element_text(size=lblfnt), legend.position="top")
 
-mod = CModel()
+mod = CModel(); mod@beamLam <- mod@decohLam
 smpl = CuSampling()
-Ttot = -2.3/mod@decohLam
+taud = -1/mod@decohLam
+taub = -1/mod@beamLam
+tau = taud/(1+taud/taub)
+Ttot = 3*tau
 
-varwT <- function(smpl, mod, Ttot){
-  simSample(smpl, mod, Ttot) -> s
-  sum(s$FIDrvt^2) -> denom
-  w = s$FIDrvt^2/denom
-  meanwT = sum(w*s$Time)
-  
-  sum(w*(s$Time - meanwT)^2)
+.ggplot_Sgl <- function(s){
+  ggplot(s[seq(1,nrow(s), length.out=300),]) + 
+    geom_line(aes(Time, XSgl), lwd=.2) + 
+    geom_point(aes(Time, Sgl), col="red", size=.5) +
+    thm +labs(y="Signal")
 }
-vwt = varwT(smpl, mod, Ttot)
+
+## SIGNAL ####
+s = simSample(smpl, mod, Ttot)
+.ggplot_Sgl(s)
+
+SNR <- function(model, x, rerr0=3e-2){
+  taud = -1/model@decohLam
+  taub = -1/model@beamLam
+  z = taub/taud
+  f = (1-2*z)/2/z
+  
+  model@Pol/rerr0 * exp(f/taud * x) # here sqrt(dte) from A and from the SNR expression cancel 
+                                    # as a result of normalizing A to have rel. error at t=0 rerr0
+}
+
+rError <- function(model, x, rerr0=3e-2) rerr0 * exp(-.5*model@beamLam * x) 
+
+## COMPACTION TIME VS W SE ####
+varwT <- function(smpl, mod, Ttot){
+  sum(smpl$FIDrvt^2) -> denom
+  w = smpl$FIDrvt^2/denom
+  meanwT = sum(w*smpl$Time)
+  
+  sum(w*(smpl$Time - meanwT)^2)
+}
+vwt = varwT(s, mod, Ttot)
 g <- function(x) {
   lam = mod@decohLam
   lamp = mod@decohLam*pi/mod@wFreq
@@ -34,15 +62,6 @@ dte = 1e-5
 SEw = 1e-4
 Xtot = (SDe/SEw)^2/vwt
 
-# f <- function(dt){
-#   w = mod@wFreq
-#   dt + sin(w*dt)/w - 2*Xtot*dte/Gtot
-# }
-# 
-# uniroot(f,c(0,1))$root -> x0
-
-# x = seq(0, 1, 1e-3)
-# plot(f(x)~x, type="l"); abline(h=0, v=x0, col="red"); abline(v=pi/mod@wFreq/2, lty=2, col="gray")
 
 as.numeric(c(1,2,5)%o%10^(-3:-6))->sew
 ldply(sew,
@@ -74,3 +93,67 @@ ggplot(sdf, aes(Time, Val)) + geom_line(lwd=.5) + thm +
   geom_segment(aes(x=0, xend=CMPT, y=mod@Num0-1500*Lev, yend=mod@Num0-1500*Lev, col=SEw), arrow = arrow(ends="last",length = unit(.05,"inches"), angle=90), data=tdf.p) +
   geom_segment(x=0, xend=prd[1], y=mod@Num0*.73, yend=mod@Num0*.73, arrow = arrow(ends="last", angle=90, length=unit(.1,"inches"))) + 
   annotate("text", x=prd[1]/2, y=mod@Num0*.75, label="Uniform sampling")
+
+## SE VS W ####
+library(doParallel)
+registerDoParallel(detectCores())
+
+llply(c(c(1.57, 3, 6.42)%o%10^(-2:1)), function(w, mod, sampling, duration){
+    mod@wFreq <- w
+    fit(mod, sampling, duration)
+  }, mod, smpl, Ttot, .parallel = TRUE, .paropts = list(.export=lsf.str(), .packages="dplyr")
+) -> stats
+stopImplicitCluster()
+
+ldply(stats, function(e) e["w", c("Estimate","Std. Error")]) -> stats.w
+ggplot(stats.w, aes(Estimate, `Std. Error`)) + geom_point() + 
+  thm + labs(x=expression(omega),y=expression(sigma[hat(omega)])) +
+  scale_x_log10()
+
+
+## MODULATION ####
+smpl <- CmSampling(sglFreqGuess=rnorm(1,mod@wFreq,1e-4))
+simSample(smpl, mod, Ttot) -> s
+
+s <- rbind(s0[,CMPT:="1"], s1[,CMPT:=".33"]%>%dplyr::select(-Node))
+ggplot(s[seq(1, nrow(s), length.out = 1300),]) + 
+  geom_line(aes(Time, XSgl, col=CMPT), lwd=.1) +
+  geom_point(aes(Time, Sgl, col=CMPT), size=.5) +
+  thm +
+  scale_color_manual(values=c("black","red"))
+
+
+## 
+cmpts <- seq(.1, 1, length.out=5)
+names(cmpts) <- as.character(cmpts)
+test <- function(cmpt, mod, smpl){
+  
+  mod@beamLam <- mod@beamLam*as.numeric(cmpt)
+  smpl@CMPT <- cmpt
+  
+  taud = -1/mod@decohLam
+  taub = -1/mod@beamLam
+  tau = taud/(1+taud/taub)
+  
+  simSample(smpl, mod, 3*tau) -> s
+  s[,CMPT:=as.character(cmpt)]
+  #c(Num = nrow(s), Time = s$Time[nrow(s)])
+  #fit(mod, smpl, 3*tau)
+}
+
+mod = CModel(); mod@beamLam <- mod@decohLam
+smpl <- CmSampling(sglFreqGuess=rnorm(1,mod@wFreq,1e-4))
+
+registerDoParallel(detectCores())
+ldply(cmpts, function(x, m, s) test(x, m, s), mod, smpl,
+      .parallel = TRUE, .paropts = list(.export=lsf.str(), .packages="dplyr")) -> stats
+stopImplicitCluster()
+
+
+stats <- setDT(stats)
+stats[,`:=`(Num=length(Time), Last=Time[length(Time)]), by=CMPT]
+
+ggplot(stats[seq(1,nrow(stats), length.out = 2620),]) + geom_line(aes(Time, XSgl, col=CMPT)) +thm
+
+#stats.w <- ldply(stats, function(e) e["w",1:2], .id="CMPT")
+#ggplot(stats.w) + geom_pointrange(aes(CMPT, Estimate, ymin=Estimate-`Std. Error`, ymax=Estimate+`Std. Error`))
