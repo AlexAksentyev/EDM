ed05866e5747dd0901d55ce6ba8075bc8223802e
Alex Aksentyev <alexaksentyev@gmail.com>
2017-03-09 16:47:12 +0100

    NTR

--- RScripts/Final.R
+++ RScripts/Final.R
@@ -21,8 +21,9 @@ R = CModel(Phase=pi/2); R@beamLam <- R@decohLam
 Ttot=1082
 simSample(smpl, L, Ttot, grow=TRUE) -> Ls
 simSample(smpl, R, Ttot, grow=TRUE) -> Rs
-df = rbind(Ls%>%mutate(Which="L"), Rs%>%mutate(Which="R"))
-ggplot(df[seq(1,nrow(df), length.out = 1200),]) + facet_grid(Which~.) + geom_line(aes(Time, Sgl-XSgl, col=Which), lwd=.2) + theme(legend.position="top"); rm(df)
+df = rbind(Ls%>%mutate(Which="L"), Rs%>%mutate(Which="R")); rm(Ls, Rs)
+ggplot(df[seq(1,nrow(df), length.out = 1200),]) + facet_grid(Which~.) + 
+  geom_line(aes(Time, Sgl-XSgl, col=Which), lwd=.2) + theme(legend.position="top")
 
 Alr = merge(Ls[,FIDrvt:=NULL], Rs[,FIDrvt:=NULL], by="Time", suffixes = c(".L",".R"))
 Alr[,`:=`(XVal=(XSgl.L-XSgl.R)/(XSgl.L+XSgl.R), Val=(Sgl.L-Sgl.R)/(Sgl.L+Sgl.R))]
@@ -34,4 +35,5 @@ f = Val ~ n0*exp(lam*Time)*sin(w*Time + p)
 guess = list(n0=.4, lam=-1e-4, w=3, p=-pi/2)
 nlreg(f, weights=~1/(n0*exp(lam*Time)), data=Alr, start=guess) -> mod3l
 
-mod3l%>%summary
+mod3l%>%summary -> mod3l.s
+print(mod3l.s)
