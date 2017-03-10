ac36e209a0df00bfa6ce0038d1fce8d276f0d6af
Alex Aksentyev <alexaksentyev@gmail.com>
2017-03-10 14:38:52 +0100

    Final report: simulation

--- RScripts/Final.R
+++ RScripts/Final.R
@@ -1,4 +1,5 @@
 library(nlreg)
+library(ggplot2)
 
 rm(list=ls(all=TRUE))
 
@@ -17,17 +18,19 @@ smpl = CuSampling(Freq=500)
 L = CModel(Phase=-pi/2); L@beamLam <- L@decohLam
 R = CModel(Phase=+pi/2); R@beamLam <- R@decohLam
 
-Ttot=1082
+Ttot=1000
 simSample(smpl, L, Ttot, grow=TRUE) -> Ls
 simSample(smpl, R, Ttot, grow=TRUE) -> Rs
-df = rbind(Ls%>%mutate(Which="L"), Rs%>%mutate(Which="R"))
-ggplot(df[seq(1,nrow(df), length.out = 1200),]) + facet_grid(Which~.) + 
-  geom_line(aes(Time, Sgl-XSgl, col=Which), lwd=.2) + theme(legend.position="top")
+df = rbind(Ls%>%mutate(Detector="Left"), Rs%>%mutate(Detector="Right"))
+ggplot(df[seq(1,nrow(df), length.out = 1200),]) + facet_grid(Detector~.) + theme_bw() +
+  geom_line(aes(Time, (Sgl-XSgl)/XSgl, col=Detector), lwd=.2) + theme(legend.position="top") +
+  labs(y=expression(frac(tilde(N)[0](t)-N[0](t),N[0](t))))
 
 Alr = merge(Ls[,FIDrvt:=NULL], Rs[,FIDrvt:=NULL], by="Time", suffixes = c(".L",".R")); rm(Ls, Rs)
 Alr[,`:=`(XVal=(XSgl.L-XSgl.R)/(XSgl.L+XSgl.R), Val=(Sgl.L-Sgl.R)/(Sgl.L+Sgl.R))]
-ggplot(Alr[seq(1, nrow(Alr), length.out = 4273),]) + geom_line(aes(Time, Val-XVal), lwd=.2) #plot of residuals
-  # geom_line(aes(Time, XVal), lwd=.2, col="red") + geom_point(aes(Time, Val), size=.2) # plot of signal
+ggplot(Alr[seq(1, nrow(Alr), length.out = 4273),]) + theme_bw() + 
+  # geom_line(aes(Time, Val-XVal), lwd=.2) + labs(y="Error") #plot of residuals
+   geom_line(aes(Time, XVal), lwd=.1, col="red") + geom_point(aes(Time, Val), size=.2) + labs(y="Asymmetry")# plot of signal
 
 
 f = Val ~ n0*exp(lam*Time)*sin(w*Time + p)
