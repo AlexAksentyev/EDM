ccdc5cbfac3de6fed6e1ba0040085908c03a00f9
Alex Aksentyev <alexaksentyev@gmail.com>
2017-06-27 12:12:05 +0200

    data + analysis

--- RScripts/cosysim/CWCCWSim.R
+++ RScripts/cosysim/CWCCWSim.R
@@ -5,11 +5,11 @@ library(plyr)
 library(ggplot2)
 library(cowplot)
 
-Nrev = 25000 #simulation revolutions
-Nrps = 1e6 #revolutions per second
-sigtilt = 1e-3 #increment
 
 .complete <- function(DT){
+  Nrev = attr(DT,"Parameters")$Nrev #revolutions per simulation
+  Nrps = 1e6 #revolutions per second
+  
   DT[,':='(
     ThXY = atan(Sy/Sx),
     ThXZ = atan(Sz/Sx),
@@ -43,43 +43,59 @@ sigtilt = 1e-3 #increment
   return(DR)
 }
 
-ldply(1:5, function(k){
-  Direct <- data.table(read_csv(paste0("~/git/COSYINF/test/fort.",(6+2*k))))
-  Reverse <- data.table(read_csv(paste0("~/git/COSYINF/test/fort.", (6+2*k+1))))
+ldply(1:6, function(k){
+  ddf = paste0("~/git/COSYINF/test/fort.",(6+2*k))
+  rdf = paste0("~/git/COSYINF/test/fort.",(6+2*k+1))
+  
+  para = read_csv(ddf, n_max=1)
+  Direct <- data.table(read_csv(ddf, skip=2))
+  attr(Direct, "Parameters")<-para
+  para =  read_csv(rdf, n_max=1)
+  Reverse <- data.table(read_csv(rdf, skip=2))
+  attr(Reverse, "Parameters")<-para
   
   DR <- .prep(Direct, Reverse)
     
-  DR[,sigTILT:=(k-1)*sigtilt]
+  DR[,sigTILT:=para$sigTILT]
   DR
 }) %>% data.table() -> DR
 
-DR[,sigTILT:=as.factor(DR$sigTILT)]
+fsigTILTlevels = unique(DR$sigTILT)
+fsigTILTlevels[order(fsigTILTlevels,decreasing=FALSE)]%>%formatC(2,format="e") -> fsigTILTlevels
+
+DR[,fsigTILT:=factor(DR$sigTILT,labels=fsigTILTlevels)]
 
 ##X-DGAMMA OMEGA PLOT 
-dr <- DR[,.(sigTILT, dgamma.CW, dgamma.CCW, x.CW, x.CCW, Wy.CW, Wy.CCW, Wx.CW, Wx.CCW)]
+dr <- DR[,.(fsigTILT,sigTILT, dgamma.CW, dgamma.CCW, x.CW, x.CCW, Wy.CW, Wy.CCW, Wx.CW, Wx.CCW)]
 wch = "CCW"
-ggplot(dr) + geom_point(aes_string(paste0("x.",wch), paste0("Wy.",wch), col=paste0("dgamma.",wch), shape="sigTILT")) + 
+ggplot(dr) + geom_point(aes_string(paste0("x.",wch), paste0("Wy.",wch), col=paste0("dgamma.",wch), shape="fsigTILT")) + 
   theme_bw() + labs(x="x",y=expression(Omega[y]), col=expression(Delta~gamma),shape=expression(sigma[tilt])) -> p1
-ggplot(dr) + geom_point(aes_string(paste0("x.",wch), paste0("Wx.",wch), col=paste0("dgamma.",wch), shape="sigTILT")) + 
+ggplot(dr) + geom_point(aes_string(paste0("x.",wch), paste0("Wx.",wch), col=paste0("dgamma.",wch), shape="fsigTILT")) + 
   theme_bw() + labs(x="x",y=expression(Omega[x]), col=expression(Delta~gamma),shape=expression(sigma[tilt])) -> p2
 plot_grid(p1,p2,nrow=2)
 
 ## HISTOGRAMS
-ggplot(dr) + geom_histogram(aes(Wx.CW, fill=dgamma.CW),binwidth=1e-4) + facet_grid(sigTILT~.)
+ggplot(dr) + geom_histogram(aes(Wx.CW, fill=dgamma.CW),binwidth=1e-4) + facet_grid(fsigTILT~.)
 
 ##CROSS PLOT
-filter(DR, abs(dWy)>0)%>% 
-  ggplot(aes(abs(dWy),abs(dWx), col=sigTILT))+
+filter(DR, abs(dWy)>0, abs(dWx)>0)%>% 
+  ggplot(aes(abs(dWy),abs(dWx), col=fsigTILT))+
   geom_point() + theme_bw() + theme(legend.position="top") +
   labs(x=expression(abs(Delta~Omega[y])), y=expression(abs(Sigma~Omega[x])), col=expression(sigma[tilt])) +
-  geom_smooth(method="lm", se=FALSE)
+  geom_smooth(method="lm", se=FALSE) +
+  scale_y_log10() + scale_x_log10() -> p3
 
-ddply(DR, .(sigTILT), function(df){
+ddply(DR, .(fsigTILT), function(df){
   lm(abs(dWx)~abs(dWy), df, abs(dWy)>0) -> m
   coef(summary(m))[1:2, 1:2]-> mt
-  c(Itct=mt[1,1],SEItct=mt[1,2],Slp=mt[2,1],SESlp=mt[2,2])
+  c(sigTILT=df$sigTILT[1], Itct=mt[1,1],SEItct=mt[1,2],Slp=mt[2,1],SESlp=mt[2,2])
 }) -> .stats
 
 ggplot(.stats, aes(sigTILT, Itct)) + geom_point() +
   geom_errorbar(aes(ymin=Itct-SEItct, ymax=Itct+SEItct)) +
-  theme_bw() + labs(x=expression(sigma[tilt]), y=expression(abs(Sigma~Omega[x])))
+  theme_bw() + labs(
+    x=expression(sigma[tilt]), 
+    y=expression(abs(Sigma~Omega[x])~"("~abs(Delta~Omega[y])~"=0 )")
+  ) +
+  scale_y_log10() + scale_x_log10() -> p4
+plot_grid(p3,p4,nrow = 2)
