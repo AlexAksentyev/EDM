240a8bcd6c49beedfd07dcc5dceb0ee6d014ac27
Alex Aksentyev <alexaksentyev@gmail.com>
2017-07-05 18:03:54 +0200

    i think the problem's with the equation factor

--- RScripts/cosysim/CWCCWAna_TSS.R
+++ RScripts/cosysim/CWCCWAna_TSS.R
@@ -1,31 +1,34 @@
 library(readr)
 library(lattice)
+library(ggplot2)
+library(data.table)
+library(dplyr)
+library(mosaic)
 
+## CONSTANTS ####
+clight = 299792458 #m/s
+Lcir = 145.85 #m
+e=1.602e-19 #C
+md = 2*1.672e-27 #kg
+G=-.14
+
+## INPUT ####
 
 filename = "~/git/COSYINF/test/fort.10"
-TSS2d <- data.table(read_csv(filename, skip=2))
-attr(TSS2d,"Parameter") <- read_csv(filename, n_max=1) -> para
+TSSdata <- data.table(read_csv(filename, skip=2))
+para <- read_csv(filename, n_max=1)
+setattr(TSSdata,"Parameter", para)
 
-TSS2d[,Dim:=2]; TSS3d[,`:=`(Dim=3,l=NULL)]
+Ntps = clight*para$beta/Lcir
 
-TSSdata <- rbind(TSS2d,TSS3d)
-
-D0 <- TSSdata[y==0&ERG==min(abs(ERG))]
-
-D0[,.(MEAN.Wx = mean(Wx), SD.Wx = sd(Wx), RSD.Wx = sd(Wx)/mean(Wx)), by=c("Dim","muTILT","x")] %>%
-  ggplot(aes(x, MEAN.Wx,col=as.factor(Dim))) + geom_point(show.legend = FALSE) + 
-  # geom_linerange(aes(ymin=MEAN.Wx-SD.Wx,ymax=MEAN.Wx+SD.Wx), show.legend = FALSE) +
-  facet_grid(Dim~.,scales = "free_y") + theme(legend.position="top")
+TSSdata[,`:=`(Omega=mu*Ntps, Dim=para$Dimension)]
 
 
-D0 %>%ggplot(aes(Wx)) + geom_histogram() +
-  geom_violin(aes(xintercept=mean(Wx)),col="red") + 
-  facet_grid(.~Dim, scales="free_x")
+alpha = e/md/clight*(para$gamma*G+1)/para$gamma
 
-wireframe(Wx ~ x*y, data = TSSdata[ERG==min(abs(ERG))&Dim==3],
-          xlab = "X coordinate", ylab = "Y coordinate",
-          main = "Wx",
-          drape = TRUE,
-          colorkey = TRUE,
-          screen = list(z = 15, x = -90)
-)
+
+## PLOTS ####
+
+TSSdata[x==0&y==0&ERG==min(abs(ERG))]%>%ggplot(aes(muTILT, nx))+
+  geom_point()+
+  theme_bw()
