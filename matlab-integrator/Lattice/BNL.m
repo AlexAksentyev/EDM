34ad99e56d68132d413acefbff29186499e272ea
Alex Aksentyev <alexaksentyev@gmail.com>
2017-09-11 17:19:52 +0200

    added lattice

--- matlab-integrator/Lattice/BNL.m
+++ matlab-integrator/Lattice/BNL.m
@@ -12,25 +12,100 @@ Lq = 5;
 GSFP = 0;
 GSDP = 0;
 
-drift_25 = @()emdrift(.25);
-drift_15 = @()emdrift(.15);
-drift2_2 = @()emdrift(2.2);
-bpm = @(15);
+DL_25 = @()emdrift(.25);
+DL_15 = @()emdrift(.15);
+DL2_2 = @()emdrift(2.2);
+BPM = @()emdrift(15);
 
-dquadSS1 = @()mquadr(Lq, -.86);
-fquadSS1 = @()mquadr(Lq, .831);
+QDS = @()mquadr(Lq, -.86);
+QFS = @()mquadr(Lq, .831);
 
-fquadSS2 = @()mquadr(Lq, .831);
-dquadSS2 = @()mquadr(Lq, -.86);
+QFA = @()mquadr(Lq,  1.364);
+QDA = @()mquadr(Lq,-1.023);
 
-fquadA = @()mquadr(Lq,  1.364);
-dquadrA = @()mquadr(Lq,-1.023);
+SfA = @()msext(.15, GSFP);
+SdA = @()msext(.15, GSDP);
 
-fsexA = @()msext(.15, GSFP);
-dsexA = @()msext(.15, GSDP);
+WA = @()wien(1.808, j, R, h); % i don't understand the By formula in there
 
-wienA = @()wien(Lbp, j, R, h);
+lattice = [
+        
+        %%%% SS1H2 %%%%
+              {QDS};{DL_25};{DL_15};{DL_25};{DL2_2};{DL_25};{BPM};{DL_25};
+        {QFS};{QFS};{DL_25};{DL_15}; % {RF};
+                                    {DL_25};{DL2_2};{DL_25};{BPM};{DL_25};
+        {QDS};{QDS};{DL_25};{DL_15};{DL_25};{DL2_2};{DL_25};{BPM};{DL_25};
+        {QFS};
+        
+        %%%% ARC1 %%%%
+              {QFA};{DL_25};{SfA};{DL_25};{WA};{DL_25};{BPM};{DL_25};
+        {QDA};{QDA};{DL_25};{SdA};{DL_25};{WA};{DL_25};{BPM};{DL_25};
+        {QFA};{QFA};{DL_25};{SfA};{DL_25};{WA};{DL_25};{BPM};{DL_25};
+        
+        {QDA};{QDA};{DL_25};{SdA};{DL_25};{WA};{DL_25};{BPM};{DL_25};
+        {QFA};{QFA};{DL_25};{SfA};{DL_25};{WA};{DL_25};{BPM};{DL_25};
+        
+        {QDA};{QDA};{DL_25};{SdA};{DL_25};{WA};{DL_25};{BPM};{DL_25};
+        {QFA};{QFA};{DL_25};{SfA};{DL_25};{WA};{DL_25};{BPM};{DL_25};
+        
+        {QDA};{QDA};{DL_25};{SdA};{DL_25};{WA};{DL_25};{BPM};{DL_25};
+        {QFA};{QFA};{DL_25};{SfA};{DL_25};{WA};{DL_25};{BPM};{DL_25};
+        
+        {QDA};{QDA};{DL_25};{SdA};{DL_25};{WA};{DL_25};{BPM};{DL_25};
+        {QFA};{QFA};{DL_25};{SfA};{DL_25};{WA};{DL_25};{BPM};{DL_25};
+        
+        {QDA};{QDA};{DL_25};{SdA};{DL_25};{WA};{DL_25};{BPM};{DL_25};
+        {QFA};{QFA};{DL_25};{SfA};{DL_25};{WA};{DL_25};{BPM};{DL_25};
+        
+        {QDA};{QDA};{DL_25};{SdA};{DL_25};{WA};{DL_25};{BPM};{DL_25};
+        {QFA};{QFA};{DL_25};{SfA};{DL_25};{WA};{DL_25};{BPM};{DL_25};
+        
+        {QDA};{QDA};{DL_25};{SdA};{DL_25};{WA};{DL_25};{BPM};{DL_25};
+        {QFA};
+        
+        %%%% SS2H1 %%%%
+              {QFS};{DL_25};{SfA};{DL_25};{DL2_2};{DL_25};{BPM};{DL_25};
+        {QDS};{QDS};{DL_25};{SdA};{DL_25};{DL2_2};{DL_25};{BPM};{DL_25};
+        {QFS};{QFS};{DL_25};{SfA};{DL_25};{DL2_2};{DL_25};{BPM};{DL_25};
+        
+        %%%% SS2H2 %%%%
+        {QDS};{DL_25};{DL_15};{DL_25};{DL2_2};{DL_25};{BPM};{DL_25};
+        {QFS};{QFS};{DL_25};{DL_15};{DL_25};{DL2_2};{DL_25};{BPM};{DL_25};
+        {QDS};{QDS};{DL_25};{DL_15};{DL_25};{DL2_2};{DL_25};{BPM};{DL_25};
+        {QFS};
+        
+        %%%% ARC2 %%%%
+              {QFA};{DL_25};{SfA};{DL_25};{WA};{DL_25};{BPM};{DL_25};
+        
+        {QDA};{QDA};{DL_25};{SdA};{DL_25};{WA};{DL_25};{BPM};{DL_25};
+        {QFA};{QFA};{DL_25};{SfA};{DL_25};{WA};{DL_25};{BPM};{DL_25};
+        
+        {QDA};{QDA};{DL_25};{SdA};{DL_25};{WA};{DL_25};{BPM};{DL_25};
+        {QFA};{QFA};{DL_25};{SfA};{DL_25};{WA};{DL_25};{BPM};{DL_25};
+        
+        {QDA};{QDA};{DL_25};{SdA};{DL_25};{WA};{DL_25};{BPM};{DL_25};
+        {QFA};{QFA};{DL_25};{SfA};{DL_25};{WA};{DL_25};{BPM};{DL_25};
+        
+        {QDA};{QDA};{DL_25};{SdA};{DL_25};{WA};{DL_25};{BPM};{DL_25};
+        {QFA};{QFA};{DL_25};{SfA};{DL_25};{WA};{DL_25};{BPM};{DL_25};
+        
+        {QDA};{QDA};{DL_25};{SdA};{DL_25};{WA};{DL_25};{BPM};{DL_25};
+        {QFA};{QFA};{DL_25};{SfA};{DL_25};{WA};{DL_25};{BPM};{DL_25};
+        
+        {QDA};{QDA};{DL_25};{SdA};{DL_25};{WA};{DL_25};{BPM};{DL_25};
+        {QFA};{QFA};{DL_25};{SfA};{DL_25};{WA};{DL_25};{BPM};{DL_25};
+        
+        {QDA};{QDA};{DL_25};{SdA};{DL_25};{WA};{DL_25};{BPM};{DL_25};
+        {QFA};{QFA};{DL_25};{SfA};{DL_25};{WA};{DL_25};{BPM};{DL_25};
+        
+        {QDA};{QDA};{DL_25};{SdA};{DL_25};{WA};{DL_25};{BPM};{DL_25};
+        {QFA};
+        
+        %%%% SS1H1
+              {QFS};{DL_25};{SfA};{DL_25};{DL2_2};{DL_25};{BPM};{DL_25};
+        {QDS};{QDS};{DL_25};{SdA};{DL_25};{DL2_2};{DL_25};{BPM};{DL_25};
+        {QFS};{QFS};{DL_25};{SfA};{DL_25};{DL2_2};{DL_25};{BPM};{DL_25};
+        {QDS};
 
-
-
+     ];
 
