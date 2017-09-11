66f8782f8dda4957fa9fc3241c5cdd26ae091173
Alex Aksentyev <alexaksentyev@gmail.com>
2017-09-11 15:55:40 +0200

    trying to use the default deflector formulas for now

--- matlab-integrator/Lattice/BNL.m
+++ matlab-integrator/Lattice/BNL.m
@@ -4,9 +4,9 @@ initBDT; setD;
 
 G = -.14;
 h = .05;
-R = 42.169;
+R = 9.207;
 Ebp = -12e6; %V/m
-Lbp = pi/16 * W0*g^3*b^2*G*1e12/(Ebp*(G+1));
+Lbp = pi/16 *(m*c*c/MeV)*g^3*b^2*G*1e6/(Ebp*(G+1));
 
 Lq = 5;
 GSFP = 0;
@@ -19,14 +19,17 @@ bpm = @(15);
 
 dquadSS1 = @()mquadr(Lq, -.86);
 fquadSS1 = @()mquadr(Lq, .831);
+
 fquadSS2 = @()mquadr(Lq, .831);
 dquadSS2 = @()mquadr(Lq, -.86);
 
 fquadA = @()mquadr(Lq,  1.364);
 dquadrA = @()mquadr(Lq,-1.023);
+
 fsexA = @()msext(.15, GSFP);
 dsexA = @()msext(.15, GSDP);
-wienA = @()wien(Lbp, Ebp, Bbe); %implement the wien function
+
+wienA = @()wien(Lbp, j, R, h);
 
 
 
--- matlab-integrator/setD.m
+++ matlab-integrator/setD.m
@@ -4,4 +4,4 @@ m = 3.3435832E-27;
 q = 1.60217646E-19;
 G = -0.142987167;
 
-W0=270.005183;
+W0 = 270.005183; % magical kinetic energy for the deuteron
