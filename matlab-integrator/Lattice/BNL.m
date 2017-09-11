6c93bc42d986f3bbd99885151f51b86736ae35a7
Alex Aksentyev <alexaksentyev@gmail.com>
2017-09-11 14:45:09 +0200

    implementing the BNL lattice; need to define the wien filter element

---
+++ matlab-integrator/Lattice/BNL.m
@@ -0,0 +1,33 @@
+clc; clear;
+
+initBDT; setD;
+
+G = -.14;
+h = .05;
+R = 42.169;
+Ebp = -12e6; %V/m
+Lbp = pi/16 * W0*g^3*b^2*G*1e12/(Ebp*(G+1));
+
+Lq = 5;
+GSFP = 0;
+GSDP = 0;
+
+drift_25 = @()emdrift(.25);
+drift_15 = @()emdrift(.15);
+drift2_2 = @()emdrift(2.2);
+bpm = @(15);
+
+dquadSS1 = @()mquadr(Lq, -.86);
+fquadSS1 = @()mquadr(Lq, .831);
+fquadSS2 = @()mquadr(Lq, .831);
+dquadSS2 = @()mquadr(Lq, -.86);
+
+fquadA = @()mquadr(Lq,  1.364);
+dquadrA = @()mquadr(Lq,-1.023);
+fsexA = @()msext(.15, GSFP);
+dsexA = @()msext(.15, GSDP);
+wienA = @()wien(Lbp, Ebp, Bbe); %implement the wien function
+
+
+
+
