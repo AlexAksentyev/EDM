fdb163921410b62e3a054366374f74571a44461e
AlexAksentyev <alexaksentyev@gmail.com>
2017-09-13 23:08:54 +0200

    operates somehow

--- INTEGRATOR/CParticle.py
+++ INTEGRATOR/CParticle.py
@@ -1,6 +1,6 @@
 from scipy.integrate import odeint
 import numpy as np
-
+import pandas
 
 class Element:
     
@@ -27,7 +27,12 @@ class Drift(Element):
     
     def __init__(self, Length):
         Element.__init__(self, 0, Length)
-
+        
+    
+    def EField(self, X):
+        x,y = X[0:2]
+        return (-100*x, -100*y, 0 ) # test
+    
 class Particle:
     
     _EZERO = 1.602176462e-19 # Coulomb
@@ -45,8 +50,8 @@ class Particle:
     
     def __init__(self, State0):
         
-        self._fIniState = State0
-        self.fState = State0
+        self._fIniState = list(State0)
+        self.fState = {0:list(State0)}
         
         self.fGamma0, self.fBeta0 = self._GammaBeta(self.fKinEn0)
     
@@ -58,17 +63,16 @@ class Particle:
     def _RHS(self, state, at, element):
         """ accepts field definitions in the form of a dictionary
         """
+        x,y,s,px,py,dEn,Sx,Sy,Ss = state # px, py are normalized to P0c for consistency with the other vars
         
         KinEn = self.fKinEn0*(1+dEn) # dEn = (En - En0) / En0
         lPC = lambda KNRG:  np.sqrt((self.fMass0 + KNRG)**2 - self.fMass0**2)
         
         Pc = lPC(KinEn) # momentum
-        P0c = lPC(self.KinEn0) # reference momentum
-        
-        x,y,s,px,py,dEn,Sx,Sy,Sz = state # px, py are normalized to P0c for consistency with the other vars
+        P0c = lPC(self.fKinEn0) # reference momentum
         
         Px,Py = [P0c*x for x in (px,py)] # turn px,py back to MeVs
-        Ps = np(Pc**2 - Px**2 - Py**2)
+        Ps = np.sqrt(Pc**2 - Px**2 - Py**2)
         
         Ex,Ey,Es = element.EField(state)
         Bx,By,Bs = element.BField(state)
@@ -79,23 +83,23 @@ class Particle:
         
         xp,yp = [x * hs/Ps for x in (Px,Py)]
         Wp = (Ex*xp +Ey*yp +Es) * 1e-6 # Kinetic energy prime (in MeV)
-        gammap = Wp/self_.fMass0 # gamma prime
+        gammap = Wp/self.fMass0 # gamma prime
         
         gamma,beta = self._GammaBeta(KinEn)
         v = beta*self._clight
         
-        m0 = self._fMass0/self._clight**2
+        m0 = self.fMass0/self._clight**2
         q = self._EZERO
         ## I don't understand the following formulas
-        betap = (Wp*(self.fMass0)**2)/((KinEn+self.fMass0)**2*sqrt(KinEn**2+2*KinEn*self.fMass0))
+        betap = (Wp*(self.fMass0)**2)/((KinEn+self.fMass0)**2*np.sqrt(KinEn**2+2*KinEn*self.fMass0))
         tp = H/v
         
         D = (q/(m0*hs))*(xp*By-yp*Bx+H*Es/v)-((gamma*v)/(H*hs))*3*kappa*xp # what's this?
         xpp=((-H*D)/(gamma*v))*xp+((q*H)/Pc)*(H*Ex/v+yp*Bs-hs*By)+kappa*hs
         ypp=((-H*D)/(gamma*v))*yp+((q*H)/Pc)*(H*Ey/v+hs*Bx-xp*Bs)
         
-        Pxp = Px*(betap/beta - gammap/gamma)+p*xpp/H-Px*((Px*xpp)/(Pc*H)+(Py*ypp)/(Pc*H)+(hs*kappa*xp)/(H**2))
-        Pyp = Py*(betap/beta - gammap/gamma)+p*ypp/H-Py*((Px*xpp)/(Pc*H)+(Py*ypp)/(Pc*H)+(hs*kappa*xp)/(H**2))
+        Pxp = Px*(betap/beta - gammap/gamma)+Pc*xpp/H-Px*((Px*xpp)/(Pc*H)+(Py*ypp)/(Pc*H)+(hs*kappa*xp)/(H**2))
+        Pyp = Py*(betap/beta - gammap/gamma)+Pc*ypp/H-Py*((Px*xpp)/(Pc*H)+(Py*ypp)/(Pc*H)+(hs*kappa*xp)/(H**2))
         
         t5 = tp
         t6 =  t5* (q / (gamma * m0 * self.fMass0)) * (self.fG + 1/(1 + gamma))
@@ -107,17 +111,21 @@ class Particle:
         Syp =                   t6 * ((Px * Ey - Py * Ex) * Sx - (Py * Es - Ps * Ey) * Ss) + (sp1*Bs+sp2*Ps)*Sx-(sp1*Bx+sp2*Px)*Ss
         Ssp = (-1)*kappa * Sx + t6 * ((Py * Es - Ps * Ey) * Sy - (Ps * Ex - Px * Es) * Sx) + (sp1*Bx+sp2*Px)*Sy-(sp1*By+sp2*Py)*Sx
         
-        DX = [xp, yp, tp, Pxp/P0c, Pyp/P0c, Wp/self.fMass0, Sxp, Syp, Ssp]
+        DX = [xp, yp, tp, Pxp/P0c, Pyp/P0c, Wp/self.fKinEn0, Sxp, Syp, Ssp]
         
         return DX
     
     def track(self, ElementSeq, ntimes):
-        for i in range(len(ElementSeq)):
-            element = ElementSeq[i]
-            at = np.linspace(0, element.fLength, 101)
-            
-            self.fState = element.frontKick(self.fState)
-            self.fState = odeint(self._RHS, self.fState, at, args=element)
-            self.fState = element.rearKick(self.fState)
+        brks = 101
+        Xtmp = self._fIniState
+        for n in range(1,ntimes+1):
+            for i in range(len(ElementSeq)):
+                element = ElementSeq[i]
+                at = np.linspace(0, element.fLength, brks)
+                
+                Xtmp = element.frontKick(Xtmp)
+                Xtmp = odeint(self._RHS, Xtmp, at, args=(element,))[brks-1]
+                Xtmp = element.rearKick(Xtmp)
+            self.fState.update({n:Xtmp})
         
         
