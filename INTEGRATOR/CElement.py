371cb64f95046be40f0e6581610a62ed2b871f68
Alex Aksentyev <alexaksentyev@gmail.com>
2017-09-14 14:09:58 +0200

    more elements: dipole, sextupole

--- INTEGRATOR/CElement.py
+++ INTEGRATOR/CElement.py
@@ -36,5 +36,44 @@ class MQuad(Element):
     """ magnetic quadrupole
     """
     
+    _fGrad = None
     
+    def __init__(self, Length, Grad):
+        Element.__init__(self, 0, Length)
+        self._fGrad = Grad
+        
+    def BField(self, arg):
+        x,y = arg[0:2]
+        return (-self._fGrad*x, -self._fGrad*y,0)
+        
+
+class MDipole(Element):
+    """ bending magnetic dipole (horizontally/vertically bending);
+    define _BField as a tuple;
+    could also be used as a solenoid, if _BField = (0,0,Bz)
+    """
     
+    _BField = None
+    
+    def __init__(self, Length, R, BField):
+        Element.__init__(self, 1/R, Length)
+        self._BField = BField
+        
+    def BField(self, arg):
+        return self._BField
+        
+
+class MSext(Element):
+    """ magnetic sextupole
+    """
+    
+    _fGrad = None
+    
+    def __init__(self, Length, Grad):
+        Element.__init__(self, 0, Length)
+        self._fGrad = Grad
+        
+    def BField(self, arg):
+        x,y=arg[0:2]
+        return (self._fGrad*x*y,.5*self._fGrad*(x**2 - y**2), 0)
+        
