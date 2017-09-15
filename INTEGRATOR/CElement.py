96ab3dfe392ee05356df7621f45570ae38967ee9
Alex Aksentyev <alexaksentyev@gmail.com>
2017-09-15 16:21:20 +0200

    safer acces to particle fields

--- INTEGRATOR/CElement.py
+++ INTEGRATOR/CElement.py
@@ -57,7 +57,22 @@ class MDipole(Element):
     
     def __init__(self, Length, R, BField):
         Element.__init__(self, 1/R, Length)
-        self._BField = BField
+        self.setBField(BField)
+        
+    def setBField(self,BField):
+        self._BField = BField[:]
+        
+    def getBField(self):
+        return self._BField[:]
+    
+    @staticmethod    
+    def computeBStrength(particle, R):
+        return particle.Pc(particle.fKinEn0)*1e6/R
+    
+    @staticmethod
+    def computeRaduis(particle, BField):
+        return particle.Pc(particle.fKinEn0)/(particle.CLIGHT()*1e-6*BField)
+        
         
     def BField(self, arg):
         return self._BField
@@ -78,7 +93,8 @@ class MSext(Element):
         return (self._fGrad*x*y,.5*self._fGrad*(x**2 - y**2), 0)
         
 class Wien(Element):
-    """ wien filter
+    """ wien filter ?!?! 
+    The magnetic field definition is weird
     """
     
     _R = None
@@ -90,6 +106,28 @@ class Wien(Element):
         self._R = [R, R - Hgap, R**2/(R-Hgap)]
         self._Volt = Voltage
         self._BField = BField
+        
+    @staticmethod
+    def computeVoltage(particle, R, Hgap):
+        gamma,beta = particle.GammaBeta(particle.fKinEn0)
+        v = beta*particle.CLIGHT()
+        R = [R, R-Hgap, R**2/(R-Hgap)]
+        E0 = - particle.Pc(particle.fKinEn0)*v/R[0] * 1e6
+        return (E0 * R[0] * NP.log(R[2] / R[1])) / (-2)
+    
+    @staticmethod
+    def computeBStrength(particle, R, Hgap): # no idea about the formulas here
+        x = particle[0]
+        gamma,beta = particle.GammaBeta(particle.fKinEn0)
+        v = beta*particle.CLIGHT()
+        R = [R, R-Hgap, R**2/(R-Hgap)]
+        E0 = - particle.Pc(particle.fKinEn0)*v/R[0] * 1e6
+        qm = particle.EZERO()/particle.fMass0
+        k = qm * ((2 - 3*beta**2 - .75*beta**4)/beta**2 + 1/gamma)
+        
+        B0 = -E0/v
+        k = k*1.18
+        return 0.018935*(-B0)*(-1/R + k*B0*v)*x
         
     def EField(self, arg):
         x = arg[0]
@@ -106,7 +144,9 @@ class Wien(Element):
         R2 = self._R[2]
         V = self._Volt
         u = -V + 2*V*NP.log((R+x)/R1)/NP.log(R2/R1)
-        particle._fState[5] -= u*1e-6/particle.fKinEn0
+        Xk = particle.getState()
+        Xk[5] -= u*1e-6/particle.fKinEn0
+        particle.setState(Xk)
         
     def rearKick(self, particle):
         x=particle._fState[0]
@@ -115,4 +155,6 @@ class Wien(Element):
         R2 = self._R[2]
         V = self._Volt
         u = -V + 2*V*NP.log((R+x)/R1)/NP.log(R2/R1)
-        particle._fState[5] += u*1e-6/particle.fKinEn0
+        Xk = particle.getState()
+        Xk[5] += u*1e-6/particle.fKinEn0
+        particle.setState(Xk)
