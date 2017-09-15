b5d0ed7141277d9b14b50f5b9db11ccba7f39834
Alex Aksentyev <alexaksentyev@gmail.com>
2017-09-15 14:44:33 +0200

    Wien filter test

--- INTEGRATOR/test.py
+++ INTEGRATOR/test.py
@@ -22,18 +22,19 @@ FODO = [fquad, ds_25, dquad, ds_25]
 R = 7.55
 B0 = p.Pc(p.fKinEn0)*1e6/R
 mdip = ENT.MDipole(1.8, R, (0,B0,0))
+WA = ENT.Wien(1.808,R,.05,120e5,B0)
 
 
 p.track([mdip]*3,40,FWD=False)
 
-x = [p.fState[i][0] for i in p.fState]
-y = [p.fState[i][1] for i in p.fState]
-t = [p.fState[i][2] for i in p.fState]
-dW = [p.fState[i][5] for i in p.fState]
-Sx = [p.fState[i][6] for i in p.fState]
-Sy = [p.fState[i][7] for i in p.fState]
-Ss = [p.fState[i][8] for i in p.fState]
-H = [p.fState[i][9] for i in p.fState]
+x = [p.fStateLog[i][0] for i in p.fStateLog]
+y = [p.fStateLog[i][1] for i in p.fStateLog]
+t = [p.fStateLog[i][2] for i in p.fStateLog]
+dW = [p.fStateLog[i][5] for i in p.fStateLog]
+Sx = [p.fStateLog[i][6] for i in p.fStateLog]
+Sy = [p.fStateLog[i][7] for i in p.fStateLog]
+Ss = [p.fStateLog[i][8] for i in p.fStateLog]
+H = [p.fStateLog[i][9] for i in p.fStateLog]
 
 df = PDS.DataFrame({'x':x,'y':y,'Sx':Sx,'Sy':Sy,'t':t,'H':H,'dW':dW})
 df = PDS.melt(df, id_vars=['t','H'])
