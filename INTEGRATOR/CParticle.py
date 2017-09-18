92d280ef3ab1fc0cc304848dbecdb98785d3cfac
Alex Aksentyev <alexaksentyev@gmail.com>
2017-09-18 13:52:26 +0200

    NTR

--- INTEGRATOR/CParticle.py
+++ INTEGRATOR/CParticle.py
@@ -62,33 +62,38 @@ class Particle:
         Bx,By,Bs = element.BField(state)
         
         kappa = element.fCurve
-        hs = 1 + kappa*x # don't exactly understand this
-        H = Pc*hs/Ps # or this
+        hs = 1 + kappa*x # look here http://www.iaea.org/inis/collection/NCLCollectionStore/_Public/23/011/23011647.pdf
+                            # ds' = (R+x)dphi = (1+x/R)ds = hs ds, ds = Rdphi
+                            # slope = Px/Ps = dx/ds' = x'/hs => x' = Px/Ps * hs (eq 2.6)
+        xp,yp = [x * hs/Ps for x in (Px,Py)] 
         
-        xp,yp = [x * hs/Ps for x in (Px,Py)]
-        Wp = (Ex*xp +Ey*yp +Es) * 1e-6 # Kinetic energy prime (in MeV)
-        gammap = Wp/self.fMass0 # gamma prime
+        Hp = Pc*hs/Ps # path traveled by particle
+                     # H^2 = ds'^2 + dx^2 + dy^2, dx = x' ds, dy = y' ds, ds' = (1+c*x)ds
+                     # H^2 = ds^2 hs^2 *(1 + (Px/Ps)^2 + (Py/Ps)^2) = (ds hs)^2 (Pc)^2/Ps^2
+                     # H' = Pc/Ps hs
+        
+        dEnp = (Ex*xp +Ey*yp +Es) * 1e-6 # added Kinetic energy prime (in MeV)
+        gammap = dEnp/self.fMass0 # gamma prime
         
         gamma,beta = self.GammaBeta(KinEn)
-        
         q = self._ezero
         clight = self._clight
         v = beta*clight
         m0 = q*1e6*self.fMass0/clight**2
         
-        ## I don't understand the following formulas
-        betap = (Wp*(self.fMass0)**2)/((KinEn+self.fMass0)**2*np.sqrt(KinEn**2+2*KinEn*self.fMass0))
-        tp = H/v
+        tp = Hp/v # dt = H/v; t' = dt/ds = H'/v
         
-        D = (q/(m0*hs))*(xp*By-yp*Bx+H*Es/v)-((gamma*v)/(H*hs))*3*kappa*xp # what's this?
+        ## I don't understand the following formulas
+        betap = (dEnp*(self.fMass0)**2)/((KinEn+self.fMass0)**2*np.sqrt(KinEn**2+2*KinEn*self.fMass0))
+        D = (q/(m0*hs))*(xp*By-yp*Bx+Hp*Es/v)-((gamma*v)/(Hp*hs))*3*kappa*xp # what's this?
         
         # these two are in the original dimensions
-        xpp=((-H*D)/(gamma*v))*xp+(clight*H/(Pc*1e6))*(H*Ex/v+yp*Bs-hs*By)+kappa*hs
-        ypp=((-H*D)/(gamma*v))*yp+(clight*H/(Pc*1e6))*(H*Ey/v+hs*Bx-xp*Bs)
+        xpp=((-Hp*D)/(gamma*v))*xp+(clight*Hp/(Pc*1e6))*(Hp*Ex/v+yp*Bs-hs*By)+kappa*hs
+        ypp=((-Hp*D)/(gamma*v))*yp+(clight*Hp/(Pc*1e6))*(Hp*Ey/v+hs*Bx-xp*Bs)
         
         # these two are in MeVs
-        Pxp = Px*(betap/beta - gammap/gamma)+Pc*xpp/H-Px*((Px*xpp)/(Pc*H)+(Py*ypp)/(Pc*H)+(hs*kappa*xp)/(H**2))
-        Pyp = Py*(betap/beta - gammap/gamma)+Pc*ypp/H-Py*((Px*xpp)/(Pc*H)+(Py*ypp)/(Pc*H)+(hs*kappa*xp)/(H**2))
+        Pxp = Px*(betap/beta - gammap/gamma)+Pc*xpp/Hp-Px*((Px*xpp)/(Pc*Hp)+(Py*ypp)/(Pc*Hp)+(hs*kappa*xp)/(Hp**2))
+        Pyp = Py*(betap/beta - gammap/gamma)+Pc*ypp/Hp-Py*((Px*xpp)/(Pc*Hp)+(Py*ypp)/(Pc*Hp)+(hs*kappa*xp)/(Hp**2))
         
         
         Px,Py,Ps = tuple([e/clight for e in (Px,Py,Ps)]) # the original formulas use momenta, not P*c
@@ -98,12 +103,12 @@ class Particle:
         sp1 = t5*(-q / (gamma*m0))*(1 + self.fG * gamma)
         sp2 = t5*( q / (gamma*m0**2 * self.fMass0)) * (self.fG/(1 + gamma))*(Px*Bx+Py*By+Ps*Bs)
         
-        
+        # this is probably from TBMT
         Sxp =      kappa * Ss + t6 * ((Ps * Ex - Px * Es) * Ss - (Px * Ey - Py * Ex) * Sy) + (sp1*By+sp2*Py)*Ss-(sp1*Bs+sp2*Ps)*Sy
         Syp =                   t6 * ((Px * Ey - Py * Ex) * Sx - (Py * Es - Ps * Ey) * Ss) + (sp1*Bs+sp2*Ps)*Sx-(sp1*Bx+sp2*Px)*Ss
         Ssp = (-1)*kappa * Sx + t6 * ((Py * Es - Ps * Ey) * Sy - (Ps * Ex - Px * Es) * Sx) + (sp1*Bx+sp2*Px)*Sy-(sp1*By+sp2*Py)*Sx
         
-        DX = [xp, yp, tp, Pxp/P0c, Pyp/P0c, Wp/self.fKinEn0, Sxp, Syp, Ssp, H]
+        DX = [xp, yp, tp, Pxp/P0c, Pyp/P0c, dEnp/self.fKinEn0, Sxp, Syp, Ssp, Hp]
         
         return DX
     
